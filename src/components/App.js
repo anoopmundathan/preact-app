cf0ebb925770012fdda63d5e92aa09b139145ea4
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-15 22:12:31 +0100

    Get info from github

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
-function App() {
-	return(
-		<div>
-			<h2>Hello Preact component</h2>
-		</div>
-	);
+export default class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: null,
+			loading: true
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://api.github.com/users/anoopmundathan')
+			.then(response => response.json())
+			.then(user => {
+				
+				console.log(user);
+
+				this.setState({
+					user: user,
+					loading: false
+				});	
+			})
+			.catch(err => console.log(err));
+	}
+	render() {
+		return(
+			<div>
+				{
+					this.state.loading 
+					? 
+					<h1>Loading...</h1> 
+					:
+					<div>
+						<img src={this.state.user.avatar_url} alt="avatar"/>
+						<p>{this.state.user.name}</p>
+					</div>
+				}
+
+			</div>
+		);
+	
+	}
 }
-
-export default App;
